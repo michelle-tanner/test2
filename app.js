04a6711bf2d3e1d549e1dbea589cff4ee502b2d1
Michelle <michellemt0518@gmail.com>
2025-04-01 19:22:40 -0500

    add html and styling

---
+++ app.js
@@ -0,0 +1,22 @@
+window.onmousedown = (e) => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = (e) => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
+    maxDelta = window.innerWidth / 2;   // divide by 2 since full scroll = half of screen
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    
+    track.dataset.percentage = nextPercentage;
+    
+    track.style.transform = "translate(" + {nextPercentage} + "%, -50%)";
+}
